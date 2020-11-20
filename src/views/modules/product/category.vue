bfd7532b12a9a72504650e35790c31e2aa9c8cd0
lucifer <1342547278@qq.com>
2020-11-20 17:14:20 +0800

    P52 Done

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,108 +1,162 @@
 <template>
-
-<!--  :expand-on-click-node    show-checkbox    node-key="catId" 具体功能去官网查看
+  <div>
+    <!--  :expand-on-click-node    show-checkbox    node-key="catId" 具体功能去官网查看
 第一个button按钮，v-if="node.level<=2" 这样只有1,2级菜单才能使用添加功能
 第二个button按钮，v-if="node.childNodes.length==0"  只有没有子节点的菜单才能使用删除功能
 在methods中添加了append和remove方法用来测试
 -->
-  <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox node-key="catId" :default-expanded-keys="expandedKey">
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button v-if="node.level<=2" type="text" size="mini" @click="() => append(data)">
-          Append
-        </el-button>
-        <el-button v-if="node.childNodes.length==0" type="text" size="mini" @click="() => remove(node, data)">
-          Delete
-        </el-button>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
 
+    <el-dialog title="添加分类" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory()">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-  export default {
-    comments: {},
-    props: {},
+export default {
+  comments: {},
+  props: {},
 
-    data() {
-      return {
-        menus: [],
-        expandedKey: [],
-        defaultProps: {
-          children: 'children',
-          label: 'name'
-        }
+  data() {
+    return {
+      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      dialogVisible: false,
+      menus: [],
+      expandedKey: [],
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+    };
+  },
 
-      };
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        // 注意then里面的data是{data}解构赋值！！！！
+        console.log("successly get data...", data.data);
+        return (this.menus = data.data);
+      });
     },
 
-    methods: {
-      getMenus() {
-        this.$http({
-          url: this.$http.adornUrl('/product/category/list/tree'),
-          method: 'get',
-        }).then(({data}) => {
-          // 注意then里面的data是{data}解构赋值！！！！
-          console.log("successly get data...", data.data)
-          return this.menus = data.data
-        })
-      },
+    // 点击append按钮时，弹出对话框（dialogVisible设置为true）
+    append(data) {
+      console.log("append", data);
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+    },
 
-      append(data) {
-        console.log("append", data)
-      },
+    // 点击确定按钮时，将添加的分类数据提交
+    addCategory() {
+      console.log("提交的三级分类数据", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        this.$message({
+          showClose: true,
+          message: "菜单添加成功",
+          type: "success",
+        });
+        // 关闭对话框
+        this.dialogVisible = false;
+        // 刷新菜单
+        this.getMenus();
+        // 设置默认展开的菜单
+        this.expandedKey = [this.category.parentCid];
 
-      remove(node, data) {
+      });
+    },
 
-        console.log("remove", node, data);
+    remove(node, data) {
+      console.log("remove", node, data);
 
-        var ids=[data.catId];
+      var ids = [data.catId];
 
-        // 消息弹框，但删除成功时使用弹窗提醒
-    
-        this.$confirm(`是否删除【${data.name}】菜单`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
+      // 消息弹框，但删除成功时使用弹窗提醒
+
+      this.$confirm(`是否删除【${data.name}】菜单`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
           // 点击确定之后，成功的操作
           this.$http({
-          url: this.$http.adornUrl('/product/category/delete'),
-          method: 'post',
-          data: this.$http.adornData(ids,false)
-          }).then(({data})=>{
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
             // 弹出信息框，提示删除成功在页面显示
-             this.$message({
+            this.$message({
               showClose: true,
-              message: '删除成功',
-              type: 'success'
-          });
+              message: "删除成功",
+              type: "success",
+            });
             // 在控制台显示消息
-            console.log('删除成功')
+            console.log("删除成功");
 
             // 刷新三级菜单目录
-            this.getMenus()
+            this.getMenus();
 
             // 设置默认展开的菜单
-            this.expandedKey=[node.parent.data.catId]
+            this.expandedKey = [node.parent.data.catId];
           });
-          
-        }).catch(() => {
+        })
+        .catch(() => {
           // 点击取消之后的操作，暂无
         });
-
-
-        
-        }
     },
+  },
 
-    created() {
-      this.getMenus()
-    }
-
-  }
+  created() {
+    this.getMenus();
+  },
+};
 </script>
 
 <style scoped>
