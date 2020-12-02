e66ed27abac4a7504807583cf7dcabe82fd26ccf
lucifer <1342547278@qq.com>
2020-12-02 19:25:03 +0800

    P72 Done 获取分类属性分组

---
+++ src/views/modules/common/category.vue
@@ -0,0 +1,68 @@
+<!-- TODO: P71 前端组件抽取 -->
+<template>
+  <el-tree :data="menus" :props="defaultProps" node-key="catId" ref="menuTree" @node-click="nodeClick">
+  </el-tree>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {
+      menus: [],
+      expandedKey: [],
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        // 注意then里面的data是{data}解构赋值！！！！
+        console.log("successly get data...", data.data);
+        return (this.menus = data.data);
+      });
+    },
+
+    nodeClick(data,node,component){
+      // 共三个参数，依次为：传递给 data 属性的数组中该节点所对应的对象、节点对应的 Node、节点组件本身。
+        console.log("common-category被点击了：",data,node,component);
+            // todo: P71向父组件发送事件
+            // 三个参数：名字（用于父组件接收使用）：node-click-tree;要给父组件传递的值：data,node,category
+        this.$emit("tree-node-click",data,node,component)
+
+    }
+  },
+
+  created() {
+    this.getMenus();
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped>
+/* //@import url(); 引入公共css类 */
+</style>
