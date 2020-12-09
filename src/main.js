a9a6b988e5c755abd4288ee033f78da02f19e3e3
lucifer <1342547278@qq.com>
2020-12-09 15:37:02 +0800

    P85 Done 导入PubSub依赖，设置全局引用

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import '@/assets/scss/index.scss'
 import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
 import { isAuth } from '@/utils'
 import cloneDeep from 'lodash/cloneDeep'
+import PubSub from 'pubsub-js'
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
@@ -20,6 +21,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // 挂载全局
+Vue.prototype.PubSub = PubSub
 Vue.prototype.$http = httpRequest // ajax请求方法
 Vue.prototype.isAuth = isAuth     // 权限方法
 
